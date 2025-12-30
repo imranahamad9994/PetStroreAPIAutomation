15d8f451c641630f5afe58427d90966340c3c773
imranahamad9994 <imran.ahamad9994@gmail.com>
2025-12-30 16:47:17 +0530

    All necessary Validations Added

---
+++ Run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Hp\PetStore\PetStoreAutomation
+mvn clean test
--- src/test/java/api/test/UserTest.java
+++ src/test/java/api/test/UserTest.java
@@ -9,6 +9,7 @@ import com.github.javafaker.Faker;
 import api.endpoints.UserEndPoints;
 import api.payload.User;
 import io.restassured.response.Response;
+import static org.hamcrest.Matchers.*;
 
 public class UserTest {
 	
@@ -34,6 +35,15 @@ public class UserTest {
 		
 		Response response = UserEndPoints.createUser(userPayload);
 		response.then().log().all();
+		response.then()
+						.statusCode(200)
+						.body("code", equalTo(200))
+						.body("type", equalTo("unknown"))
+						.body("message", notNullValue());
+		
+		response.then().header("Content-Type", containsString("application/json"));
+		response.then().header("Connection", "keep-alive");
+		response.then().time(lessThan(3000L));
 		
 		Assert.assertEquals(response.statusCode(), 200);
 		
@@ -44,6 +54,16 @@ public class UserTest {
 	{
 		Response response = UserEndPoints.readUser(userPayload.getUsername());
 		response.then().log().all();
+		
+		response.then().body("username", equalTo(userPayload.getUsername()))
+						.body("firstName", equalTo(userPayload.getFirstName()))
+						.body("lastName", equalTo(userPayload.getLastName()))
+						.body("email", equalTo(userPayload.getEmail()))
+						.body("id", notNullValue());
+		
+		//verify data consistency
+		String username = response.jsonPath().getString("username");
+		Assert.assertEquals(username, userPayload.getUsername());
 		
 		Assert.assertEquals(response.getStatusCode(), 200);
 		
@@ -58,8 +78,22 @@ public class UserTest {
 		userPayload.setEmail(faker.internet().safeEmailAddress());
 		
 		Response response = UserEndPoints.updateUser(userPayload.getUsername(), userPayload);
+		
 		response.then().log().all();
+		
+		response.then().body("code", equalTo(200))
+						.body("type", equalTo("unknown"))
+						.body("message", notNullValue());
+		
 		Assert.assertEquals(response.getStatusCode(), 200);
+		
+		//Validate updated data using GET API (MANDATORY)
+		Response getUpdateResponse =  UserEndPoints.readUser(userPayload.getUsername());
+		
+		getUpdateResponse.then().body("firstName", equalTo(userPayload.getFirstName()))
+								.body("lastName", equalTo(userPayload.getLastName()))
+								.body("email", equalTo(userPayload.getEmail()));
+	
 	}
 	
 	@Test(priority =4)
